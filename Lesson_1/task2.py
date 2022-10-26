b42b9c51ad555d86dda37c42fd187048632a85e7
TkalychMaksim <tkalychmaksim@gmail.com>
2022-10-26 19:48:26 +0300

    add 1 task

---
+++ Lesson_1/task2.py
@@ -0,0 +1,34 @@
+import math
+#Import library "Math" to  work with square root
+print("To work with the program, enter 2 numbers and select an operation from the list")
+print("For operations ^,√ enter only one number, instead of the second number enter '0'")
+print("To enter √ sign use the key combination (alt+251");
+firstnumber = int(input("Input first number:"));
+secondnumber = int(input("Input second number:"));
+#Request numbers from user and translation to int
+action = input("Choose the operation from list(+,-,*,/,^,√)");
+#Reuest action from user
+
+
+if action == "+" :
+     result = firstnumber + secondnumber
+     print("Result: " + str(result))
+
+if action == "-":
+    result = firstnumber - secondnumber
+    print("Result: " + str(result))
+
+if action == "*":
+    result = firstnumber * secondnumber
+    print("Result: " + str(result))
+
+if action == "/":
+    result = firstnumber / secondnumber
+    print("Result: " + str(result))
+
+if action == '^':
+    result = firstnumber * firstnumber
+    print("Result:" + str(result))
+if action == "√":
+    result = math.sqrt(firstnumber)
+    print("Result:  " + str(result))
