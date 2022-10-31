fafcd1b7dc027733076b45f1b5f9af22c2ec1592
TkalychMaksim <tkalychmaksim@gmail.com>
2022-10-31 21:03:26 +0200

    add fixed task4

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -1,5 +1,4 @@
-import math
-#Import necessary Library
+
 while True:
     first_coef = int(input('Input first coefficient of equation:'))
     second_coef = int(input('Input second coefficient of equation:'))
@@ -22,12 +21,10 @@ while True:
         print("Your equation:", str(first_coef) + 'x^2', str(second_coef) + 'x', str(third_coef), '=0')
         print('============================')
 #Loop for correct representation of the equation
-    discriminant = math.pow(second_coef, 2)-(4*first_coef * third_coef)
+    discriminant = (second_coef ** 2)-(4*first_coef * third_coef)
     if (discriminant<0):
         print('Equation Error: discriminant<0')
     else:
-        print(discriminant)
-        print(math.sqrt(discriminant))
-        x1 = ((-second_coef) - math.sqrt(float(discriminant))) / (2 * first_coef)
-        x2 = ((-second_coef) + math.sqrt(float(discriminant))) / (2 * first_coef)
+        x1 = ((-second_coef) - (float(discriminant ** 0.5))) / (2 * first_coef)
+        x2 = ((-second_coef) + (float(discriminant ** 0.5))) / (2 * first_coef)
         print('Your answers:', x1, x2)
