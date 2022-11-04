bf74816c1225d00514343ee4b527e3f137f130da
TkalychMaksim <tkalychmaksim@gmail.com>
2022-11-05 00:33:40 +0200

    add task5

---
+++ Lesson_2/task5.py
@@ -0,0 +1,16 @@
+input_list = input('Enter your list: ').split(',')
+#List entry
+int_list = []
+for element in input_list:
+    int_list.append(int(element))
+#translation of the list for the correct operation of the max, min functions
+print('The maximum value is: ' + str(max(int_list)))
+print('The minimum value is: ' + str(min(int_list)))
+
+int_list.sort()
+summ = 0
+for element in int_list:
+    if element != int_list[0] and element != int_list[-1]:
+        summ = summ + element
+print('Sum equals: ', summ)
+
