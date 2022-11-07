b4f8398587c58aaefb46c89a3349494ba2df9f2f
TkalychMaksim <tkalychmaksim@gmail.com>
2022-11-07 21:57:28 +0200

    add task4

--- Lesson_2/task4.py
+++ Lesson_2/task4.py
@@ -2,5 +2,5 @@ number = int(input('Enter a number to calculate the factorial: '))
 factorial = 1
 while number > 1:
     factorial *= number
-    number = number -1
+    number -= 1
 print(factorial)
