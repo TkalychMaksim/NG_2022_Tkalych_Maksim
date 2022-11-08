70913c9a83d68698889e08a1579197d418ceaf2f
TkalychMaksim <tkalychmaksim@gmail.com>
2022-11-08 20:58:21 +0200

    add modified task4

--- Lesson_1/task4.py
+++ Lesson_1/task4.py
@@ -3,24 +3,6 @@ while True:
     first_coef = int(input('Input first coefficient of equation:'))
     second_coef = int(input('Input second coefficient of equation:'))
     third_coef = int(input('Input third coefficient of equation:'))
-
-    if (second_coef>0) and (third_coef>0):
-        print('============================')
-        print("Your equation:", str(first_coef) + 'x^2', '+' + str(second_coef) + 'x', '+' + str(third_coef), '=0')
-        print('============================')
-    elif (second_coef<0) and (third_coef>0):
-        print('============================')
-        print("Your equation:", str(first_coef) + 'x^2', str(second_coef) + 'x', '+' + str(third_coef), '=0')
-        print('============================')
-    elif (second_coef>0) and (third_coef<0):
-        print('============================')
-        print("Your equation:", str(first_coef) + 'x^2', '+' + str(second_coef) + 'x', str(third_coef), '=0')
-        print('============================')
-    elif (second_coef<0) and (third_coef<0):
-        print('============================')
-        print("Your equation:", str(first_coef) + 'x^2', str(second_coef) + 'x', str(third_coef), '=0')
-        print('============================')
-#Loop for correct representation of the equation
     discriminant = (second_coef ** 2)-(4*first_coef * third_coef)
     if (discriminant<0):
         print('Equation Error: discriminant<0')
