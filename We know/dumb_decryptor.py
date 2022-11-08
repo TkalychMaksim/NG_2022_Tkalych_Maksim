70355f553a43dce92901ce64ccad5d40e60be928
TkalychMaksim <tkalychmaksim@gmail.com>
2022-11-08 20:44:43 +0200

    add modified decryptor

--- We know/dumb_decryptor.py
+++ We know/dumb_decryptor.py
@@ -3,12 +3,12 @@ input_text = input('Input text to decoding: ').upper()
 #Entering text with uppercase translation to avoid errors
 encoder_step = int(input('Choose the step of encoder: '))
 decoded_text = ''
-for i in input_text:
-    place = alphabet.find(i)
+for letter in input_text:
+    place = alphabet.find(letter)
     new_place = place + encoder_step
-    if i in alphabet:
+    if letter in alphabet:
         decoded_text += alphabet[new_place]
     else:
-        decoded_text += i
+        decoded_text += letter
 print(decoded_text)
 
